90f544a11ca08ab11c567601aadccc64faa9056c
RomanZateev <roma-zateev@mail.ru>
2019-11-04 14:33:38 +0700

    added req

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const token = '1046385267:AAF7FCr8oDTmHBgZY05BqAvQG_QckKits8g'
 
 const bot = new TelegramBot(token, {polling: true});
 
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})
+
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `Hola ${msg.contact.first_name} ${msg.contact.last_name}`);
 })
