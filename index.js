beab360e0406cc4831dd87608c1ef93213aa4ad5
RomanZateev <roma-zateev@mail.ru>
2019-11-04 15:05:59 +0700

    added proxy server

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '1046385267:AAF7FCr8oDTmHBgZY05BqAvQG_QckKits8g'
 
-const bot = new TelegramBot(token, {polling: true});
+var Agent = require('socks5-https-client/lib/Agent');
+
+const bot = new TelegramBot(token, 
+    {
+        polling: true,
+        request: {
+            agentClass: Agent,
+            agentOptions: {
+                socksHost: '185.232.20.215',
+                socksPort: '1080'
+            }
+        }
+    });
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('')
 })
+
+
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `Hola ${msg.contact.first_name} ${msg.contact.last_name}`);
